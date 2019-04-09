a4153308e94d65e60175f4c11be06cc0319c7236
Paulo Morais <paulomorais@localhost.localdomain>
2019-04-09 19:42:50 -0300

    Commit periodico

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get("/teste", (req, res) => {
-    return res.send("ola mundo");
+    return res.send("ola word");
 });
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:<password>@cluster0-8a1im.mongodb.net/omnistack?retryWrites=true',
+    {
+        useNewUrlParser: true
+    }
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
