6b5809629ecf6196c8228ff0049ed5c859f3a73b
pkthegod <pkthegod@users.noreply.github.com>
2019-04-12 01:52:39 -0300

    Commit periódico 120401

---
+++ frontend_aparencia/src/pages/Main/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import logo from "../../assets/logo.svg";
+import './styles.css';
+
+export default class Main extends Component {
+    render() {
+    return (
+        <div id="main-container">
+            <form action="">
+                <img src={logo} alt=""/>
+                <input placeholder="Criar Box" />
+                <button type="submit">Criar</button>
+            </form>
+        </div>
+    );
+  }
+}
