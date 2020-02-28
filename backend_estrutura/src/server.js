25596ef13326140afa4c4a7f6b5edf67e7f72700
pkthegod <pkthegod@users.noreply.github.com>
2020-02-28 09:54:24 -0300

    Update 2802

--- backend_estrutura/src/server.js
+++ backend_estrutura/src/server.js
@@ -1,37 +1,10 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-const cors = require("cors");
+const express = require('express')
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+app.use(require('./routes'))
 
-io.on('connection', socket => {
-    socket.on('connectRoom', box => {
-        socket.join(box);
-    })
-})
-
-mongoose.connect('mongodb+srv://omnistack:pktoor0807@cluster0-8a1im.mongodb.net/omnistack?retryWrites=true',
-    {
-        useNewUrlParser: true
-    }
-);
-
-app.use((req, res, next) => {
-    req.io = io;
-
-    return next();
-});
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
-
-app.use(require('./routes'));
-
-server.listen(process.env.PORT || 3333);
+app.listen(3333)
